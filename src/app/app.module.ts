98fa0ac4047f0f5bf6465d86631c053d392ec53b
jp-freitas <tec.joao.freitas@gmail.com>
2023-03-20 19:31:12 -0300

    Implementing navigation, dashboard, hero's details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
+    HeroDetailComponent,
     MessagesComponent
   ],
   imports: [
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
@@ -15,5 +16,11 @@ export class HeroService {
     const heroes = of(HEROES);
     this.messageService.add('Hero Service: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(hero => hero.id === id)!;
+    this.messageService.add(`Hero Service: fetched hero id=${id}`);
+    return of(hero)
   }
 }
